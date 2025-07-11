08b4a49c26507bae7d6e31afaba587e796ea1665
Rafli Malik <Rafli.Malik@gmail.com>
2025-07-11 16:21:03 +0700

    chore: add dark mode

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+	import SunIcon from "@lucide/svelte/icons/sun";
+	import MoonIcon from "@lucide/svelte/icons/moon";
+
+	import { resetMode, setMode } from "mode-watcher";
+	import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+	import { buttonVariants } from "$lib/components/ui/button/index.js";
+
 	const link = [
 		{
 			name: "Home",
@@ -8,11 +15,15 @@
 			name: "Compare",
 			url: "/compare",
 		},
+		{
+			name: "Gallery",
+			url: "/gallery",
+		},
 	];
 </script>
 
 <nav
-	class="fixed z-50 flex w-full items-center bg-white/90 px-4 py-2 shadow-sm backdrop-blur-md sm:px-6 lg:px-8 xl:px-10"
+	class="fixed z-50 flex h-fit w-full items-center justify-between border-b bg-background/80 px-4 py-2 shadow-xs backdrop-blur-md sm:px-6 lg:px-8 xl:px-10"
 >
 	<ul class="flex gap-2">
 		{#each link as item (item.url)}
@@ -25,4 +36,20 @@
 			</li>
 		{/each}
 	</ul>
+	<DropdownMenu.Root>
+		<DropdownMenu.Trigger class={buttonVariants({ variant: "outline", size: "icon" })}>
+			<SunIcon
+				class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+			/>
+			<MoonIcon
+				class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</DropdownMenu.Trigger>
+		<DropdownMenu.Content align="end">
+			<DropdownMenu.Item onclick={() => setMode("light")}>Light</DropdownMenu.Item>
+			<DropdownMenu.Item onclick={() => setMode("dark")}>Dark</DropdownMenu.Item>
+			<DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
 </nav>
