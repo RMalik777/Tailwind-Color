5a6cac38be940d610a89885e1269cd6ed0e9c1cf
Rafli Malik <Rafli.Malik@gmail.com>
2025-07-11 20:27:44 +0700

    feat: add github link in navbar

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { page } from "$app/state";
+	import { siGithub } from "simple-icons";
 	import SunIcon from "@lucide/svelte/icons/sun";
 	import MoonIcon from "@lucide/svelte/icons/moon";
 
 	import { resetMode, setMode } from "mode-watcher";
 	import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-	import { buttonVariants } from "$lib/components/ui/button/index.js";
+	import { buttonVariants, Button } from "$lib/components/ui/button/index.js";
 
 	const link = [
 		{
@@ -45,20 +46,33 @@
 			</li>
 		{/each}
 	</ul>
-	<DropdownMenu.Root>
-		<DropdownMenu.Trigger class={buttonVariants({ variant: "outline", size: "icon" })}>
-			<SunIcon
-				class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
-			/>
-			<MoonIcon
-				class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
-			/>
-			<span class="sr-only">Toggle theme</span>
-		</DropdownMenu.Trigger>
-		<DropdownMenu.Content align="end">
-			<DropdownMenu.Item onclick={() => setMode("light")}>Light</DropdownMenu.Item>
-			<DropdownMenu.Item onclick={() => setMode("dark")}>Dark</DropdownMenu.Item>
-			<DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
-		</DropdownMenu.Content>
-	</DropdownMenu.Root>
+	<div class="space-x-1">
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger class={buttonVariants({ variant: "outline", size: "icon" })}>
+				<SunIcon
+					class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+				/>
+				<MoonIcon
+					class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+				/>
+				<span class="sr-only">Toggle theme</span>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content align="end">
+				<DropdownMenu.Item onclick={() => setMode("light")}>Light</DropdownMenu.Item>
+				<DropdownMenu.Item onclick={() => setMode("dark")}>Dark</DropdownMenu.Item>
+				<DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+		<Button
+			variant="ghost"
+			size="icon"
+			class="dark:fill-white"
+			href="https://github.com/RMalik777/Tailwind-Color"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+			{@html siGithub.svg}
+		</Button>
+	</div>
 </nav>
