50f2d2135361517a8b5be299527f35f836106dde
Rafli Malik <Rafli.Malik@gmail.com>
2026-04-21 11:26:35 +0700

    chore: update component

--- src/lib/components/ui/alert/alert-action.svelte
+++ src/lib/components/ui/alert/alert-action.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import { cn, type WithElementRef } from "$lib/utils.js";
 	import type { HTMLAttributes } from "svelte/elements";
+	import { cn, type WithElementRef } from "$lib/utils.js";
 
 	let {
 		ref = $bindable(null),
 		class: className,
 		children,
 		...restProps
-	}: WithElementRef<HTMLAttributes<HTMLDivElement>> & {} = $props();
+	}: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
 </script>
 
 <div
 	bind:this={ref}
-	data-slot="select-label"
-	class={cn("px-1.5 py-1 text-xs text-muted-foreground", className)}
+	data-slot="alert-action"
+	class={cn("absolute top-2 right-2", className)}
 	{...restProps}
 >
 	{@render children?.()}
